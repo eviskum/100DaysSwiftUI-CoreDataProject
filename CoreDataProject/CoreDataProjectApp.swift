18fc381c6ba0bc832e7d04c7338ba5fc949972e5
Esben Viskum <viskum@viskum.dk>
2021-05-05 13:53:13 +0200

    Initial Commit

---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,20 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Esben Viskum on 05/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
